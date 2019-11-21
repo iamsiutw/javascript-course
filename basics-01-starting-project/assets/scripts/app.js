491cdc7c63eeb51ec7a73c5ae4d2066d96c1cd33
yamsiu <mar6861214@gmail.com>
2019-11-21 08:17:45 +0800

    realize defer & async

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0
 let currentResult = defaultResult;
+let logEntries = [];
 
 // Gets input from input field
 function getInputNumber() {
@@ -12,33 +13,48 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   outputResult(currentResult, calcDescription); // from vendor.js
 }
 
+function writeToLog(operationIdentifier, prevResult, calcNumber, newResult) {
+  const logEntry = {
+    operator: operationIdentifier,
+    prevResult: prevResult,
+    number: calcNumber,
+    result: newResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
 // Operator function
 function add() {
   const calcNumber = getInputNumber();
   const initialNumber = currentResult
-  currentResult = currentResult + calcNumber;
-  createAndWriteOutput('+', initialNumber, calcNumber)
+  currentResult += calcNumber;
+  createAndWriteOutput('+', initialNumber, calcNumber);
+  writeToLog('ADD', initialNumber, calcNumber, currentResult);
 }
 
 function subtract() {
   const calcNumber = getInputNumber();
   const initialNumber = currentResult
-  currentResult = currentResult - calcNumber;
+  currentResult -= calcNumber;
   createAndWriteOutput('-', initialNumber, calcNumber)
+  writeToLog('SUBTRACT', initialNumber, calcNumber, currentResult);
 }
 
 function multiply() {
   const calcNumber = getInputNumber();
   const initialNumber = currentResult
-  currentResult = currentResult * calcNumber;
-  createAndWriteOutput('*', initialNumber, calcNumber)
+  currentResult *= calcNumber;
+  createAndWriteOutput('*', initialNumber, calcNumber);
+  writeToLog('MULTIPLY', initialNumber, calcNumber, currentResult);
 }
 
 function divide() {
   const calcNumber = getInputNumber();
   const initialNumber = currentResult
-  currentResult = currentResult / calcNumber;
-  createAndWriteOutput('/', initialNumber, calcNumber)
+  currentResult /= calcNumber;
+  createAndWriteOutput('/', initialNumber, calcNumber);
+  writeToLog('DEVIDED', initialNumber, calcNumber, currentResult);
 }
 
 addBtn.addEventListener('click', add);
